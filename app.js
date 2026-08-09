65a08a906bcb1b2e3edf5df1123ace7dc8accecf
naiburrahman45 <naiburrahman45@gmail.com>
2026-08-09 08:18:53 -0700

    adding js

---
+++ app.js
@@ -0,0 +1,5 @@
+// element //
+const input = document.getElementById(todoInput);
+const addBtn  = document.getElementById("addbtn");
+const list    = document.getElementById("todoList");
+const count   = document.getElementById("count");
